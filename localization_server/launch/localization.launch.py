b5348ac30a7f24cb5df7aa6a8d0c8ccdb7f432d3
Pedro Araujo <araujopedrop@gmail.com>
2024-06-28 03:03:52 +0000

    Localitation_server added and tested

--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -1,14 +1,16 @@
 import os
+
 from launch_ros.actions          import Node
 from launch                      import LaunchDescription
 from launch.actions              import DeclareLaunchArgument
-from ament_index_python.packages import get_package_share_directory
 from launch.substitutions        import LaunchConfiguration, TextSubstitution, PathJoinSubstitution
+
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     
-    # RVIZ Configuration
-    rviz_config_dir = os.path.join(get_package_share_directory("map_server"), "config", 'mapper_rviz_config.rviz')
+    nav2_yaml = os.path.join(get_package_share_directory('localization_server'), 'config', 'amcl_config.yaml')
+    rviz_config_dir = os.path.join(get_package_share_directory("localization_server"), "config", 'localizer_rviz_config.rviz')
 
     map_file_default = "warehouse_map_sim.yaml"
 
@@ -17,13 +19,14 @@ def generate_launch_description():
     )
 
     # Define the base directory
-    base_dir = os.path.join(get_package_share_directory('map_server'), "config")
+    base_dir = os.path.join(get_package_share_directory('localization_server'), "config")
 
     # Concatenate the base directory with the file name
     map_file_path = PathJoinSubstitution([
         TextSubstitution(text=base_dir),
         LaunchConfiguration('map_file')
     ])
+
 
     return LaunchDescription([
         Node(
@@ -36,13 +39,22 @@ def generate_launch_description():
                        ]),
 
         Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
+
+
+        Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager_mapper',
             output='screen',
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
-                        {'node_names': ['map_server']}]),
+                        {'node_names': ['map_server','amcl']}]),
 
         Node(
             package='rviz2',
@@ -50,7 +62,7 @@ def generate_launch_description():
             output='screen',
             name='rviz_node',
             parameters=[{'use_sim_time': True}],
-            arguments=['-d', rviz_config_dir]),            
+            arguments=['-d', rviz_config_dir]),
 
-        map_file_arg,
+        map_file_arg,            
         ])
